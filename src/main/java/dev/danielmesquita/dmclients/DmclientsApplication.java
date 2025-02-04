3215b5274d0517fe190811d392315490d0468a83
Daniel <danielmesquitadev@gmail.com>
2025-02-04 17:42:10 -0300

    feat: first commit

---
+++ src/main/java/dev/danielmesquita/dmclients/DmclientsApplication.java
@@ -0,0 +1,13 @@
+package dev.danielmesquita.dmclients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmclientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmclientsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/danielmesquita/dmclients/DmclientsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.danielmesquita.dmclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmclientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
